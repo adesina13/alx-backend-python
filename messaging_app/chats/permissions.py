c5e8bbdbaa3a842bc2dd4c659f2c38d5a005b812
daadesina <daadesina1@gmail.com>
2025-11-23 14:37:15 +0100

    Task 3 done

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,13 +1,20 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsParticipantOfConversation(BasePermission):
+class IsParticipantOfConversation(permissions.BasePermission):
     """
-    Only authenticated users who are participants in the conversation
-    can access/modify messages.
+    Allows access only to authenticated users who are participants
+    in the conversation or in the related message's conversation.
     """
+
+    def has_permission(self, request, view):
+        # Only authenticated users allowed overall
+        return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        # obj can be a Conversation or a Message
-        conversation = getattr(obj, "conversation", obj)
+        # obj can be a Conversation or Message
+        if hasattr(obj, "participants"):
+            conversation = obj
+        else:
+            conversation = obj.conversation
 
         return request.user in conversation.participants.all()
