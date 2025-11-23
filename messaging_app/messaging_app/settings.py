ea598b2bd0804f7247396751f5e7751700e1abd6
daadesina <daadesina1@gmail.com>
2025-11-23 14:50:40 +0100

    Task 3 done

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -64,9 +64,11 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
 
-    "DEFAULT_PAGINATION_CLASS": "chats.pagination.MessagePagination",
-    "PAGE_SIZE": 20,   # Default page size for pagination
-    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend"
+    ],
 }
 
 
